98232154b6a2e358bbf24d94bec90543a69c7073
terd-ferguson <terd-ferguson@users.noreply.github.com>
2017-08-15 20:18:52 -0500

    Update debug.c

--- debug.c
+++ debug.c
@@ -23,11 +23,16 @@ Void debugUpdateValues()
 Void checkDebug()
 {
     unsigned int sw = GPIO_read(Board_BUTTON0);
+    unsigned int sw1 = GPIO_read(Board_BUTTON1);
 
     //Pressed is apparently 0 for the on board buttons
     if (sw == 0)
     {
         activeUpdateValues = &debugUpdateValues;
+    }
+    else if(sw1 == 0)
+    {
+        activeUpdateValues = &calibrationMode;
     }
     else
     {
